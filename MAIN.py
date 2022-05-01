6b4738bd27b48ef9a7d94f17a523e175be051d54
captaindrewsa <captaindrewsa@gmai.com>
2022-05-01 18:36:30 +0200

    болванка

--- MAIN.py
+++ MAIN.py
@@ -1,2 +1,17 @@
 import sqlite3 as sq
-import aiogram
+from unittest import skip
+from aiogram import Bot, Dispatcher, executor, types
+import logging
+
+bot = Bot(token='5371492482:AAHPyA2Ag-X9G-6h9qtE0TXolf7NCGtMcV4')
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+
+@dp.message_handler(commands="test1")
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+
+if __name__== "__main__":
+    executor.start_polling(dp, skip_updates=True)
